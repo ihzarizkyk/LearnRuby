e5debe1a4e752c21d5b4cffc7a3352a9b0d19c6a
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 15:46:49 +0700

    Update operator.rb

--- operator.rb
+++ operator.rb
@@ -5,3 +5,70 @@ Author : Mochammad Ihza Rizky Karim
 filename : operator.rb
 =end
 
+# Operator Aritmatika
+
+a = 10
+b = 20
+c = 30
+
+puts("OPERATOR ARITMATIKA")
+puts("Hasil a + b ", a+b)
+puts("Hasil a - b ", a-b)
+puts("Hasil a / b ", a/b)
+puts("Hasil a * b ", a*b)
+puts("Hasil a ** b ", a**b)
+puts("Hasil a % b ", a%b)
+
+# Operator Perbandingan
+puts("OPERATOR PERBANDINGAN")
+puts("Hasil a > b ", a>b)
+puts("Hasil a < b ", a<b)
+puts("Hasil a >= b ", a>=b)
+puts("Hasil a == b ", a==b)
+
+# Operator Assignment
+puts("OPERATOR ASSIGNMENT")
+puts("Hasil a += b ", a+=b)
+puts("Hasil a -= b ", a-=b)
+puts("Hasil a *= b ", a*=b)
+puts("Hasil a /= b ", a/=b)
+puts("Hasil a = b ", a=b)
+
+=begin
+OUTPUT :
+
+OPERATOR ARITMATIKA
+Hasil a + b 
+30
+Hasil a - b 
+-10
+Hasil a / b 
+0
+Hasil a * b 
+200
+Hasil a ** b 
+100000000000000000000
+Hasil a % b 
+10
+OPERATOR PERBANDINGAN
+Hasil a > b 
+false
+Hasil a < b 
+true
+Hasil a >= b 
+false
+Hasil a == b 
+false
+OPERATOR ASSIGNMENT
+Hasil a += b 
+30
+Hasil a -= b 
+10
+Hasil a *= b 
+200
+Hasil a /= b 
+10
+Hasil a = b 
+20
+
+=end
