e237dfbb643d66975c320cbeb47727226aac72ce
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 16:34:57 +0700

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,57 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : array.rb
+=end
+
+a = [1,2,3,4]
+b = [5,6,7,8]
+c = a + b
+
+puts a # mengambil semua nilai
+
+# mengambil salah satu data
+puts a[0]
+puts a[1]
+puts a[2]
+puts a[3]
+
+# mengambil index pertama
+
+puts a.first
+
+# mengambil index terakhir
+
+puts a.last
+
+# penambahan array
+
+puts c
+
+=begin 
+OUTPUT :
+
+1
+2
+3
+4
+
+1
+2
+3
+4
+
+1 (first)
+4 (last)
+
+penambahan array
+
+1
+2
+3
+4
+5
+6
+7
+8
+
+=end
