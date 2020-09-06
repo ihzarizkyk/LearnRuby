7e7670e2e4065c717568e6dc65793108a83b8cdd
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 19:16:23 +0700

    Update ternary.rb

--- ternary.rb
+++ ternary.rb
@@ -3,6 +3,9 @@ Author : Mochammad Ihza Rizky Karim
 filename : ternary.rb
 =end
 
+a = 10
+c = 5
+
 # ternary operator (shortcode dari if else statement)
 
 hasil = a>c ? "a lebih dari c" : "a lebih kecil dari c"
