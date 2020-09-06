0f4d6f6b5e713ad0d6fbc89392f673f15279c139
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 22:31:16 +0700

    Update user-input.rb

--- user-input.rb
+++ user-input.rb
@@ -3,7 +3,10 @@ Author : Mochammad Ihza Rizky Karim
 filename : input.rb 
 =end
 
-puts "Siapa nama anda ? "
+puts "Siapa Nama anda ? "
 nama = gets
+puts "Berapa usia anda ? "
+usia = gets
 
-puts "Nama Saya " + nama 
+puts "Nama Saya " + nama
+puts "Usia Saya " + usia + " Tahun"
