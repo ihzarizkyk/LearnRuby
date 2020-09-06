36b4a485105e1cc7e6804efbd5a48939cfeb0bc6
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 19:14:41 +0700

    Update if-elsif-else.rb

--- if-elsif-else.rb
+++ if-elsif-else.rb
@@ -1,1 +1,24 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : if-elsif-else.rb 
+=end
 
+a = 10
+b = 10
+c = 5
+
+if a > b
+	puts "a lebih dari b"
+elsif a == b
+	puts "a samadengan b"
+else
+	puts "a lebih kecil dari b"
+end
+
+=begin
+
+OUTPUT :
+
+a samadengan b
+ 
+=end 
