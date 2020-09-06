dad1638c5625d2beeffc02bf07454bb7a87b8b77
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 22:39:10 +0700

    Create function.rb

---
+++ function.rb
@@ -0,0 +1,15 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : function.rb
+=end
+
+def tambah(angka1, angka2)
+	puts angka1 + angka2
+end
+
+def kurang(angka1,angka2)
+	puts angka1 - angka2
+end
+
+tambah(1,2)
+kurang(1,1)
