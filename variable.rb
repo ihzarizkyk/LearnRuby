4dbf00036dc0d14ed1970b016df8dd5455368356
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 09:27:39 +0700

    Create variable.rb

---
+++ variable.rb
@@ -0,0 +1,29 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : variable.rb
+=end
+
+# Variabel
+
+x = 10
+y = 11
+namaDepan = "Ihza"
+namaBelakang = "Rizky"
+
+# Print Variable
+
+puts x+y,x-y,x*y
+puts namaDepan + namaBelakang
+puts namaDepan + " " + namaBelakang
+
+=begin
+
+Output :
+
+21
+-1
+110
+IhzaRizky
+Ihza Rizky
+
+=end
