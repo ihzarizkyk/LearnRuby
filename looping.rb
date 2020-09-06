a139e96bf3adf35cc8903baa23bd71da9bd235e5
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-06 21:45:52 +0700

    Create looping.rb

---
+++ looping.rb
@@ -0,0 +1,21 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : looping.rb 
+=end
+
+# While Loop
+
+i = 4
+
+while i >= 1
+	puts "looping"
+	i = i - 1
+end
+
+# For Loop
+
+arr = [1,2,3,4]
+
+for i in arr do
+	puts i
+end
