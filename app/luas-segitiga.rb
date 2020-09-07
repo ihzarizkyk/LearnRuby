a4bb5517a732d75f8758d3b1b463e638eaf83bdb
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-07 13:40:09 +0700

    Create luas-segitiga.rb

---
+++ app/luas-segitiga.rb
@@ -0,0 +1,10 @@
+puts "PENGHITUNG LUAS SEGITIGA"
+
+puts "Masukkan Alas : "
+alas = gets.to_i
+puts "Masukkan Tinggi : "
+tinggi = gets.to_i
+
+luas = 1/2*(alas*tinggi)
+
+puts("Hasil Luas Segitiga : ",luas)
