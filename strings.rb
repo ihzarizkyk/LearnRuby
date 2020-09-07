76f678ecea74ccbf097428c84019ea8c6c5ca1c3
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-07 17:28:42 +0700

    Create strings.rb

---
+++ strings.rb
@@ -0,0 +1,26 @@
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : strings.rb 
+=end
+
+kalimat = "ini adalah string"
+
+# menghitung jumlah teks
+
+puts kalimat.size # 17
+
+# merubah teks menjadi kapital
+
+puts kalimat.upcase # INI ADALAH STRING
+
+# merubah string ke integer
+
+puts kalimat.to_i # 0
+
+# merubah string ke string
+
+puts kalimat.to_s # ini adalah string
+
+# merubah string ke char
+
+puts kalimat.chars  # [i,n,i a,d,a,l,a,h s,t,r,i,n,g]
