fc6034eb85ba53fc31ff4a1659edf0456408b3b4
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-07 13:39:31 +0700

    Create kalkulator.rb

---
+++ app/kalkulator.rb
@@ -0,0 +1,12 @@
+puts "KALKULATOR SEDERHANA"
+
+puts "Masukkan Angka Pertama : "
+bil1 = gets.to_i
+puts "Masukkan Angka Kedua : "
+bil2 = gets.to_i
+
+puts("Hasil Penjumlahan bilangan : #{bil1+bil2}")
+puts("Hasil Pengurangan bilangan : #{bil1-bil2}")
+puts("Hasil Pembagian bilangan : #{bil1/bil2}")
+puts("Hasil Perkalian bilangan : #{bil1*bil2}")
+puts("Hasil Pangkat bilangan : #{bil1**bil2}")
