a786153de0e9735bbdb8dfc539eb47cb949ae693
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-08 18:15:30 +0700

    Update oop3.rb

--- oop/oop3.rb
+++ oop/oop3.rb
@@ -3,30 +3,45 @@ Author : Mochammad Ihza Rizky Karim
 filename : oop3.rb 
 =end
 
-# Getter dan Setter
+# Setter dan Getter
 
 class Orang
 
 	def initialize(nama,usia)
-		@nama = nama
-		@usia = usia
+
+	@nama = nama
+	@usia = usia
+
 	end
 
-	# Atribut Accessor Getter
+	# setter
 	#attr_reader :nama
 	#attr_reader :usia
 
-	# Atribut Accessor Setter
+	# getter
 	#attr_writer :nama
-	#attr_writer :usia	
+	#attr_writer :usia
 
-	# Atribut Accessor (Getter & Setter)
+	# accessor mutator (gabungan setter dan getter)
 	attr_accessor :nama
 	attr_accessor :usia
 
 end
 
-orang1 = Orang.new "Ihza",19
+orang1 = Orang.new "ihza",19
+orang2 = Orang.new "Rizky",19
 
 puts orang1.nama
 puts orang1.usia
+
+puts orang2.nama,orang2.usia
+
+=begin
+
+Jika Menggunakan setter dan getter manual, tidak ringkas
+dan dengan accessor mutator akan menjadi lebih ringkas
+karena menggabungkan setter dan getter 
+
+hasilnya juga sama saja... bedanya kalau accessor lebih ringkas
+
+=end
