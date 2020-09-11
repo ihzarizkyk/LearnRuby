98c075e619e1c6a589bbe7b4c9239edb227223f7
Mochammad Ihza Rizky Karim <56474969+ihzarizkyk@users.noreply.github.com>
2020-09-11 08:19:29 +0700

    Update oop5.rb

--- oop/oop5.rb
+++ oop/oop5.rb
@@ -2,3 +2,56 @@
 Author : Mochammad Ihza Rizky Karim
 filename : oop5.rb
 =end
+
+=begin
+Author : Mochammad Ihza Rizky Karim
+filename : ruby3.rb
+=end
+
+# OOP Ruby
+
+# membuat kelas
+class Orang
+
+	def initialize
+		puts "ini kelas orang"
+	end
+# membuat method
+	def sifat
+		puts "ini sifat dari kelas orang"
+	end
+  
+end
+
+# membuat object atau instansiasi
+orang1 = Orang.new
+
+# mencetak objek dengan methodnya
+puts orang1.sifat()
+
+# membuat kelas turunan
+class Anak < Orang
+
+	def initialize
+		puts "ini kelas anak"
+	end
+
+	def sifatAnak
+		puts "ini sifat kelas anak"
+	end
+  
+end
+
+# membuat object atau instansiasi
+anak1 = Anak.new
+
+# mencetak objek dengan methodnya
+puts anak1.sifatAnak()
+
+=begin 
+OUTPUT : 
+ini kelas orang
+ini sifat dari kelas orang
+ini kelas anak
+ini sifat kelas anak
+=end
